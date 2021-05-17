a184ebb20a15d7f3b368b0ed9a0f80655950744e
ZogopZ <zoiszogop@gmail.com>
2021-05-18 01:38:26 +0200

    2048 development v_0.03
    - Added the random 2 or 4 number generator.
    - The random number can only be inserted in an empty tile.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,38 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            // boardState: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            boardState: [2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2]
         }
         this.onKeyPress = this.onKeyPress.bind(this);
     }
 
+    findRandomEmptySpot() {
+        
+    }
+
+    randomGenerator() {
+        let boardState = [...this.state.boardState];
+        // Finds all zero elements of the boardState array and returns
+        // their indexes to the emptySpots array.
+        const emptySpots = this.state.boardState
+            .map((element, index) => element === 0 ? index : undefined)
+            .filter(element => element);
+        // Picks a random element from the emptySpots array.
+        // This element will be the index of the boardState's empty spot.
+        const randomSpot = Math.floor(Math.random() * emptySpots.length);
+        // Place a random 2 or 4 generated with a 90% and a 10% percent
+        // probability respectively to the boardState's chosen empty spot.
+        boardState[emptySpots[randomSpot]] = (Math.random() > 0.1 ? 2 : 4);
+        this.setState({boardState});
+    }
+
     onKeyPress(event) {
-        console.log(event.keyCode);
+        console.log("You just pressed " + event.keyCode);
+        /*
+         * Code goes here!
+         */
+        this.randomGenerator();
     }
 
     componentDidMount() {
